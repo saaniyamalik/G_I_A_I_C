675e6d3a67bb38102ab0e76d983afa3773726d95
Saniya Malik <168172365+saaniyamalik@users.noreply.github.com>
2025-01-03 13:51:40 -0800

    Add files via upload

---
+++ Assigment_3/src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+export default function Contact () {
+
+    return (
+
+        <div className="flex-col items-center text-center px-4 py-16 space-y-12">
+        <button className="square-full h-[100px] border text-black hover:bg-purple-400 hover:text-black">
+         CONTACT ME
+         </button> 
+        <h1 className="hover: bg-grey-800 text-4xl font-bold h-[5px] hover: text-white">CONTACT NUMBER : 03052762669</h1>
+        
+         </div>
+ 
+    )
+}
---
+++ Assigment_3/src/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+
+    return (
+
+        <p className="Footer">&copy; This page is created using Next-JS</p>
+    )
+}
+
